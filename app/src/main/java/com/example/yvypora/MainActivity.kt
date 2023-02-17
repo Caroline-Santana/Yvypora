f8fe8358ddfc8807718f69140501bf32bb70855f
22102358 <carolinesantana309@gmail.com>
2023-02-17 11:22:54 -0300

    commit tela principal

--- app/src/main/java/com/example/yvypora/MainActivity.kt
+++ app/src/main/java/com/example/yvypora/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.example.yvypora
 
-import android.graphics.Color
+
+import android.graphics.fonts.Font
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.yvypora.ui.theme.YvyporaTheme
@@ -28,27 +31,32 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                 ) {
-
                     Image(painter =  painterResource(id = R.drawable.background), contentDescription = "",
                         contentScale = ContentScale.Crop
                     )
                     Column(
                         modifier = Modifier
                             .fillMaxSize(),
-                        verticalArrangement = Arrangement.Center
+                        horizontalAlignment = Alignment.End,
+                        verticalArrangement = Arrangement.Top
+
                     ){
                         Text(
-                            text = stringResource(id = R.string.slogan),
-                            color = androidx.compose.ui.graphics.Color.White,
-                            fontSize = 45.sp,
-
+                            modifier = Modifier.padding(top = 35.dp, end = 10.dp),
+                            text = stringResource(id = R.string.yvypora),
+                            color = colorResource(id = R.color.green_yvy),
+                            fontSize = 32.sp,
 
                         )
+
+
                     }
 
 
                 }
+                MainLayout()
             }
+
         }
     }
 }
@@ -57,15 +65,30 @@ class MainActivity : ComponentActivity() {
 fun MainLayout(){
     Column(
         modifier =
-        Modifier.fillMaxSize()
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
     ) {
-        //Header
+        //Main
             Text(
-                text = stringResource(id = R.string.yvypora),
-                fontSize = 32.sp,
-
+                text = stringResource(id = R.string.slogan),
+                fontSize = 45.sp,
+                color = androidx.compose.ui.graphics.Color.White,
                 letterSpacing = 4.sp
             )
         }
+
+    Button(onClick = { /*TODO*/ },
+        modifier = Modifier
+            .width(50.dp)
+            .height(50.dp),
+        shape = RoundedCornerShape(5.dp),
+        colors = ButtonDefaults.buttonColors(Color(83,141,34))
+    ){
+        Text(
+            text = stringResource(id = R.string.login),
+            color = Color.White
+        )
+    }
+        
 
 }
--- app/src/main/java/com/example/yvypora/ui/theme/Type.kt
+++ app/src/main/java/com/example/yvypora/ui/theme/Type.kt
@@ -1,10 +1,14 @@
 package com.example.yvypora.ui.theme
 
+
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.yvypora.R
+
+//val best = FontFamily(Font(R.font.))
 
 // Set of Material typography styles to start with
 val Typography = Typography(
