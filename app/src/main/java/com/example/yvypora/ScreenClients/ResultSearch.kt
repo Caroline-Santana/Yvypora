5fe08dbecb18c99e9bdfeaf22732e74ace710138
22102358 <carolinesantana309@gmail.com>
2023-05-10 11:22:25 -0300

    "implementation navigation"

--- app/src/main/java/com/example/yvypora/ScreenClients/ResultSearch.kt
+++ app/src/main/java/com/example/yvypora/ScreenClients/ResultSearch.kt
@@ -20,11 +20,15 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.yvypora.R
+import com.example.yvypora.models.MarketerData
+import com.example.yvypora.models.ProductCardSale
 import com.example.yvypora.ui.theme.YvyporaTheme
 
 class ResultSearch : ComponentActivity() {
@@ -46,11 +50,73 @@ class ResultSearch : ComponentActivity() {
 
 @Composable
 fun ResultSearchMain() {
+    var nameProductSearch = "Abobrinha"
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxWidth()
+    ) {
+        CampResultSearch()
+        Spacer(modifier = Modifier.height(15.dp))
+        Text(
+            text = stringResource(id = R.string.title_result_search) + " $nameProductSearch",
+            modifier = Modifier.padding(start = 20.dp),
+            color = colorResource(id = R.color.green_options),
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(5.dp))
+        ListOfProductData(products = listProductsData())
 
-    CampResultSearch()
-
+    }
 
 }
+
+
+
+fun listProductsData() = listOf<ProductCardSale>(
+
+    ProductCardSale(
+        id = 1,
+        name = "Manga",
+        qntd_product = 3,
+        photo = 1,
+        type_weight = "g",
+        weight_product = 800,
+        price = 24.00,
+        promo = false
+    ),
+    ProductCardSale(
+        id = 1,
+        name = "Manga",
+        qntd_product = 3,
+        photo = 1,
+        type_weight = "g",
+        weight_product = 800,
+        price = 24.00,
+        promo = true,
+    ),
+    ProductCardSale(
+        id = 1,
+        name = "Manga",
+        qntd_product = 3,
+        photo = 1,
+        type_weight = "g",
+        weight_product = 800,
+        price = 24.00,
+        promo = true,
+    ),
+    ProductCardSale(
+        id = 1,
+        name = "Manga",
+        qntd_product = 3,
+        photo = 1,
+        type_weight = "g",
+        weight_product = 800,
+        price = 24.00,
+        promo = false,
+    )
+)
 
 @Composable
 fun CampResultSearch() {
@@ -58,8 +124,9 @@ fun CampResultSearch() {
         mutableStateOf("")
     }
     val context = LocalContext.current
-    Column( modifier = Modifier
-        .padding(top = 10.dp, start = 10.dp)
+    Column(
+        modifier = Modifier
+            .padding(top = 10.dp, end = 10.dp)
     ) {
         Image(
             painter = painterResource(id = R.drawable.arrow),
@@ -73,70 +140,59 @@ fun CampResultSearch() {
             alignment = Alignment.BottomStart,
             contentDescription = stringResource(id = R.string.back_screen)
         )
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(50.dp),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Row {
-                OutlinedTextField(
-                    modifier = Modifier
-                        .width(350.dp)
-                        .height(48.dp)
-                        .padding(start = 25.dp, end = 10.dp),
-                    value = searchState,
-                    onValueChange = { newText ->
-                        searchState = newText
-                    },
-                    keyboardOptions = KeyboardOptions(
-                        keyboardType = KeyboardType.Text,
-                        imeAction = ImeAction.Search
-                    ),
-                    colors = TextFieldDefaults.textFieldColors(
-                        textColor = Color.White,
-                        backgroundColor = colorResource(id = R.color.green_yvy),
-                        focusedIndicatorColor = colorResource(id = R.color.green_yvy),
-                        unfocusedIndicatorColor = colorResource(id = R.color.green_yvy),
-                        cursorColor = Color.White
-                    ),
-                    shape = RoundedCornerShape(20.dp),
-                    trailingIcon = {
-                        Icon(
-                            painter = painterResource(R.drawable.lupa_icon),
-                            contentDescription = stringResource(id = R.string.lupa),
-                            modifier = Modifier
-                                .width(35.dp)
-                                .height(35.dp)
-                                .padding(end = 10.dp)
-                                .clickable {},
-                            tint = Color.White
-                        )
-                    }
-                )
-            }
-            Column(
-//            modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Bottom
-            ) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(48.dp)
-                        .padding(end = 5.dp)
-                        .background(
-                            color = colorResource(id = R.color.green_width),
-                            shape = RoundedCornerShape(5.dp)
-                        ),
-                    contentAlignment = Alignment.Center
-                ) {
+        Row(modifier = Modifier.padding(top = 10.dp)) {
+            OutlinedTextField(
+                modifier = Modifier
+                    .width(330.dp)
+                    .height(48.dp)
+                    .padding(start = 15.dp, end = 10.dp),
+                value = searchState,
+                onValueChange = { newText ->
+                    searchState = newText
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Search
+                ),
+                colors = TextFieldDefaults.textFieldColors(
+                    textColor = Color.White,
+                    backgroundColor = colorResource(id = R.color.green_yvy),
+                    focusedIndicatorColor = colorResource(id = R.color.green_yvy),
+                    unfocusedIndicatorColor = colorResource(id = R.color.green_yvy),
+                    cursorColor = Color.White
+                ),
+                shape = RoundedCornerShape(20.dp),
+                trailingIcon = {
                     Icon(
-                        painter = painterResource(id = R.drawable.filter),
-                        contentDescription = "",
+                        painter = painterResource(R.drawable.lupa_icon),
+                        contentDescription = stringResource(id = R.string.lupa),
+                        modifier = Modifier
+                            .width(35.dp)
+                            .height(35.dp)
+                            .padding(end = 10.dp)
+                            .clickable {},
                         tint = Color.White
                     )
                 }
+            )
+            Box(
+                modifier = Modifier
+                    .width(56.dp)
+                    .height(48.dp)
+                    .padding(end = 5.dp)
+                    .background(
+                        color = colorResource(id = R.color.green_width),
+                        shape = RoundedCornerShape(5.dp)
+                    ),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.filter),
+                    contentDescription = "",
+                    tint = Color.White
+                )
             }
+
 
         }
     }
